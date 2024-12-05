e968534c5f1fbb03bee3703df6adf1c9c45f7fbb
Mert <88289398+Deta0ne@users.noreply.github.com>
2024-12-05 19:25:52 +0300

    Added Zustand to project and created Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,43 @@ import { Button } from '@/components/ui/button';
 import { ModeToggle } from '@/components/mode-toggle';
 import LanguageSwitcher from '@/components/LanguageSwitcher';
 import { useTranslation } from 'react-i18next';
+import { SidebarProvider, SidebarTrigger } from './components/ui/sidebar';
+import { SidebarInset } from './components/ui/sidebar';
+import { Separator } from './components/ui/separator';
+import { AppSidebar } from './components/app-sidebar';
+import { useCounterStore } from './store/store';
 
 function App() {
     const { t } = useTranslation();
+    const count = useCounterStore((state) => state.count);
+    const increment = useCounterStore((state) => state.increment);
+    const decrement = useCounterStore((state) => state.decrement);
+    const incrementAsync = useCounterStore((state) => state.incrementAsync);
+
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4">{t('common.title')}</h1>
-            <div className="flex gap-4 items-center">
-                <Button>{t('common.add')}</Button>
-                <ModeToggle />
-                <LanguageSwitcher />
-            </div>
-        </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <h1>{t('common.title')}</h1>
+                </header>
+                <div className="flex flex-1 flex-col gap-4 p-4">
+                    <div>
+                        <div className="flex gap-4 items-center">
+                            <Button>{t('common.add')}</Button>
+                            <ModeToggle />
+                            <LanguageSwitcher />
+                            <p>{count}</p>
+                            <Button onClick={() => increment()}>Increment</Button>
+                            <Button onClick={() => decrement()}>Decrement</Button>
+                            <Button onClick={() => incrementAsync()}>Increment Async</Button>
+                        </div>
+                    </div>
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
     );
 }
 
